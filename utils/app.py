527455e94dee65d02f537ef2e6b5882061144ee5
Coder Sathy <69349524+CoderSathy@users.noreply.github.com>
2020-10-17 17:33:13 +0545

    removed firefox and added automatic driver install

--- utils/app.py
+++ utils/app.py
@@ -1,4 +1,4 @@
-from selenium.webdriver import Chrome, Firefox
+from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -6,6 +6,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
+
+'''
+  Webdriver downloading according too chrome version ( hey nobody uses firefox so i am removing it from everywhere)
+'''
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 # 1- to enable
@@ -20,8 +25,8 @@ option.add_experimental_option("prefs", {
 
 
 class Bot:
-    def __init__(self, email, password, webd=Chrome):
-        self.driver = webd(options=option)
+    def __init__(self, email, password):
+        self.driver = Chrome(ChromeDriverManager().install())  # automatically install chrome driver acc to os default chrome version
         self.email = email
         self.password = password
 
@@ -35,15 +40,15 @@ class Bot:
 
     def message_to(self, username):
         self.driver.get(f'https://www.facebook.com/messages/t/{username}')
-        time.sleep(5)
+        time.sleep(20)    # showing regular TIMEOUT in 5 secs so changed it to 20 secs
         try:
             with open('text.txt', 'r') as file:
                 for word in file.readlines():
-                    run = WebDriverWait(self.driver, 5).until(
+                    run = WebDriverWait(self.driver, 20).until(
                         EC.presence_of_element_located((By.CLASS_NAME, '_1mf'))).send_keys(word, Keys.RETURN)
                     print(word)
-        except TimeoutException:
-            print("Something went wrong. Please try again.")
+        except TimeoutException:     #please check this its always giving TimeOuts errors
+            print("TimeoutException:  Something went wrong. Please try again.")
 
     def close_browser(self):
         self.driver.close()
